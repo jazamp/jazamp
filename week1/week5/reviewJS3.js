59ecb7c0a5c3684582234eba81e1308bcfbc4378
Titigon <64309010030@tnk.ac.th>
2022-11-17 09:42:41 +0700

    Create reviewJS3.js

---
+++ week1/week5/reviewJS3.js
@@ -0,0 +1,8 @@
+let obj = {
+    'studentID' : 64309010030,
+    'name':'sam',
+    'lastname':'Titigon',
+    'grade':3.00,
+    'Tel':[0986756788]
+}
+console.log(obj.name,obj.lastname,obj.grade,obj.Tel[0])
