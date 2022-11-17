1610f8bba200311a745c8694dce23c6c82295d89
Titigon <64309010030@tnk.ac.th>
2022-11-17 09:42:33 +0700

    Create Ex1.js

---
+++ week1/week5/Ex1.js
@@ -0,0 +1,15 @@
+let item = {
+    "A4paper" : 120,
+    "Ruler" : 25,
+    "FlashDrive" : 130,
+    "Pencil" : 80
+}
+
+//Mr.Lee
+console.log("Mr.lee", item.A4paper * 5 + item.Ruler * 2)
+
+//Mr.Yue
+console.log("Mr.Yue", item.FlashDrive * 2 + item.A4paper * 3)
+
+//Mr.fuxsexrok
+console.log("Mr.fuxsexrok", item.Pencil * 5 + item.Ruler * 2 + item.FlashDrive * 3)
